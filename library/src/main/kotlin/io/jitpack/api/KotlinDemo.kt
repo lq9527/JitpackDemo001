d7b509efc7c43666eefee46c5180c4bfea06e344
liqiang32 <liqiang32@58ganji.com>
2019-10-15 19:27:46 +0800

    init

---
+++ library/src/main/kotlin/io/jitpack/api/KotlinDemo.kt
@@ -0,0 +1,15 @@
+package com.lq.studyapplication.kotlin
+
+/**
+ * @author  liqiang
+ * @date  2019-08-02 17:26
+ * @des
+ */
+
+object KotlinDemo {
+
+    fun test() {
+        println("plugin kotlin")
+    }
+
+}
